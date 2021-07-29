988934a3956bfa794fa4553380fac2e09108b77c
Joel Jaimon <joel.jaimon99@gmail.com>
2021-07-30 00:45:06 +0530

    Image, File component added

--- whatsapp-client/src/components/ChatContainerBody/ChatContainerBody.tsx
+++ whatsapp-client/src/components/ChatContainerBody/ChatContainerBody.tsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { dropDownContext } from "../../context/dropDownContext";
 import s from "./chatContainerBodyStyles.module.scss";
+import { File } from "./components/Messages/File";
+import { Picture } from "./components/Messages/Picture";
 import { Text } from "./components/Messages/Text";
 import { Voice } from "./components/Messages/Voice";
 
@@ -26,19 +28,36 @@ export const ChatContainerBody = () => {
       <div className={s.LeftWrap}>
         <Text type="left" />
       </div>
-
+      <div className={s.RightWrap}>
+        <Voice type="right" />
+      </div>
+      <div className={s.RightWrap}>
+        <File type="right" />
+      </div>
       <div className={s.RightWrap}>
         <Text type="right" />
       </div>
-
-      <div className={s.RightWrap}>
-        <Voice type="right" />
-      </div>
-      <div className={s.RightWrap}>
-        <Voice type="right" />
-      </div>
       <div className={s.LeftWrap}>
         <Voice type="left" />
+      </div>
+      <div className={s.RightWrap}>
+        <Picture
+          type="right"
+          src={
+            "https://images.unsplash.com/photo-1521676259650-675b5bfec1ae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
+          }
+        />
+      </div>
+      <div className={s.LeftWrap}>
+        <File type="left" />
+      </div>
+      <div className={s.LeftWrap}>
+        <Picture
+          type="left"
+          src={
+            "https://images.unsplash.com/photo-1627573002203-abedce71ddb6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+          }
+        />
       </div>
     </div>
   );
--- whatsapp-client/src/components/ChatContainerBody/components/Messages/Picture.tsx
+++ whatsapp-client/src/components/ChatContainerBody/components/Messages/Picture.tsx
@@ -1,3 +1,30 @@
-export const Picture = () => {
-  return <div></div>;
+import s from "./messages.module.scss";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { useState } from "react";
+
+export const Picture = ({ type, src }: any) => {
+  const [imageOrientation, setImageOrientation] = useState<any>(null);
+  const handleImageType = (e: any) => {
+    if (e.target.naturalHeight > e.target.naturalWidth) {
+      setImageOrientation("potrait");
+    } else {
+      setImageOrientation("landscape");
+    }
+  };
+  return (
+    <span className={type === "right" ? s.imgRight : s.imgLeft}>
+      <div className={s.img}>
+        <div
+          className={
+            imageOrientation === "potrait" ? s.imgPotrait : s.imgLandscape
+          }
+        >
+          <div className={s.smoke}>
+            <ExpandMoreIcon />
+          </div>
+          <img src={src} alt="file-thumbnail" onLoad={handleImageType} />
+        </div>
+      </div>
+    </span>
+  );
 };
