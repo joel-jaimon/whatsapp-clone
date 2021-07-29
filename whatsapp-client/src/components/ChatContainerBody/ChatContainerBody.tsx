ee1c931905ccfc555385a4b040b12d74b242fe87
Joel Jaimon <joel.jaimon99@gmail.com>
2021-07-29 20:00:36 +0530

    voice msg component added

--- whatsapp-client/src/components/ChatContainerBody/ChatContainerBody.tsx
+++ whatsapp-client/src/components/ChatContainerBody/ChatContainerBody.tsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { dropDownContext } from "../../context/dropDownContext";
 import s from "./chatContainerBodyStyles.module.scss";
+import { Text } from "./components/Messages/Text";
+import { Voice } from "./components/Messages/Voice";
 
 export const ChatContainerBody = () => {
   const { dropMenu, setDropMenu } = useContext(dropDownContext);
@@ -20,6 +22,24 @@ export const ChatContainerBody = () => {
         });
       }}
       className={s.chatContainerBody}
-    ></div>
+    >
+      <div className={s.LeftWrap}>
+        <Text type="left" />
+      </div>
+
+      <div className={s.RightWrap}>
+        <Text type="right" />
+      </div>
+
+      <div className={s.RightWrap}>
+        <Voice type="right" />
+      </div>
+      <div className={s.RightWrap}>
+        <Voice type="right" />
+      </div>
+      <div className={s.LeftWrap}>
+        <Voice type="left" />
+      </div>
+    </div>
   );
 };
--- whatsapp-client/src/components/ChatContainerBody/components/Messages/Text.tsx
+++ whatsapp-client/src/components/ChatContainerBody/components/Messages/Text.tsx
@@ -1,3 +1,14 @@
-export const Text = () => {
-  return <div></div>;
+import s from "./messages.module.scss";
+
+export const Text = ({ type }: any) => {
+  return (
+    <span className={type === "right" ? s.textRight : s.textLeft}>
+      <p className={s.text}>
+        Hey guys! I have been working on this app lately, so I would really
+        appreciate if you could download this app from google playstore. This
+        app is now live. Please give your valuable feedback and don't forget to
+        rate it if you like.
+      </p>
+    </span>
+  );
 };
