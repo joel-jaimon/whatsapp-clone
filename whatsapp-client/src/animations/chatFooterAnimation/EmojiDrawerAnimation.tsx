b4d91f69f18ad4f06bde14f2189546f19163c871
Joel Jaimon <joel.jaimon99@gmail.com>
2021-09-07 18:01:24 +0530

    [fix] chat footer

--- whatsapp-client/src/animations/chatFooterAnimation/EmojiDrawerAnimation.tsx
+++ whatsapp-client/src/animations/chatFooterAnimation/EmojiDrawerAnimation.tsx
@@ -1,28 +1,28 @@
 import { animated as a, useSpring } from "@react-spring/web";
 
 export const EmojiDrawerAnimation = ({
-    children,
-    className,
-    reverse,
-    onClose,
+  children,
+  className,
+  reverse,
+  onClose,
 }: any) => {
-    const style = useSpring({
-        config: {
-            mass: 1,
-            tension: 200,
-        },
-        onRest: onClose,
-        // onChange: (e: any) => console.log(e),
-        to: {
-            height: reverse ? 0 : 300,
-        },
-        from: {
-            height: 0,
-        },
-    });
-    return (
-        <a.div style={style} className={className}>
-            {children}
-        </a.div>
-    );
+  const style = useSpring({
+    config: {
+      mass: 1,
+      tension: 200,
+    },
+    onRest: onClose,
+    // onChange: (e: any) => console.log(e),
+    to: {
+      height: reverse ? 0 : 300,
+    },
+    from: {
+      height: 0,
+    },
+  });
+  return (
+    <a.div style={style} className={className}>
+      {children}
+    </a.div>
+  );
 };
--- whatsapp-client/src/components/ChatContainerFooter/ChatContainerFooter.tsx
+++ whatsapp-client/src/components/ChatContainerFooter/ChatContainerFooter.tsx
@@ -4,17 +4,17 @@ import { Activity } from "../Activity/Activity";
 import s from "./chatContainerFooterStyles.module.scss";
 import SendIcon from "@material-ui/icons/Send";
 import {
-    AttachmentIcon,
-    AvatarIcon,
-    CameraIcon,
-    CloseIcon,
-    DocumentIcon,
-    GifIcon,
-    MicIcon,
-    PictureIcon,
-    SmileIcon,
-    StickerIcon,
-    VideoCallIcon,
+  AttachmentIcon,
+  AvatarIcon,
+  CameraIcon,
+  CloseIcon,
+  DocumentIcon,
+  GifIcon,
+  MicIcon,
+  PictureIcon,
+  SmileIcon,
+  StickerIcon,
+  VideoCallIcon,
 } from "./icons/ChatFooterIcons";
 import { ShowAttachmentAnimations } from "../../animations/chatFooterAnimation/ShowAttachmentAnimations";
 import { ExpandOptions } from "../../animations/ExpandOptions";
@@ -22,250 +22,242 @@ import { RecorderAnimation } from "../../animations/chatFooterAnimation/Recorder
 
 import { connect } from "react-redux";
 import {
-    addAttachments,
-    resetFileAttachmentModal,
-    setAttachmentModal,
+  addAttachments,
+  resetFileAttachmentModal,
+  setAttachmentModal,
 } from "../../redux/reducers/attachmentModal";
 
 const passStateFromProps = ({ chatState, attachmentModal }: any) => ({
-    activeChat: chatState.chat[chatState.activeChat],
-    attachmentModal,
+  activeChat: chatState.chat[chatState.activeChat],
+  attachmentModal,
 });
 
 const passDispatchToProps = (dispatch: any) => ({
-    setAttachmentModal: (modal: any) => dispatch(setAttachmentModal(modal)),
-    addAttachments: (files: any[]) => dispatch(addAttachments(files)),
-    resetAttachmentModal: () => dispatch(resetFileAttachmentModal(null)),
+  setAttachmentModal: (modal: any) => dispatch(setAttachmentModal(modal)),
+  addAttachments: (files: any[]) => dispatch(addAttachments(files)),
+  resetAttachmentModal: () => dispatch(resetFileAttachmentModal(null)),
 });
 
 export const ChatContainerFooter = connect(
-    passStateFromProps,
-    passDispatchToProps
+  passStateFromProps,
+  passDispatchToProps
 )(
-    ({
-        setAttachmentModal,
-        addAttachments,
-        activeChat,
-        attachmentModal,
-        resetAttachmentModal,
-    }: any) => {
-        const [height, setHeight] = useState(0);
-        const [activity, setActivity] = useState<boolean | string>(false);
-        const [recording, setRecording] = useState(false);
-        const [attachmentMenu, setAttachmentMenu] = useState(false);
-        const [reverseRecordingAnimation, setReverseRecording] =
-            useState(false);
-        const [
-            reverseAnimationAttachmentMenu,
-            setReverseAnimationAttachmentMenu,
-        ] = useState(false);
-        const [reverseActivityAnimation, setReverseActivityAnimation] =
-            useState(false);
-        const [typing, setTyping] = useState(false);
-        const [input, setInput] = useState("");
-        const inputRef = useRef(null);
+  ({
+    setAttachmentModal,
+    addAttachments,
+    activeChat,
+    attachmentModal,
+    resetAttachmentModal,
+  }: any) => {
+    const [height, setHeight] = useState(0);
+    const [activity, setActivity] = useState<boolean | string>(false);
+    const [recording, setRecording] = useState(false);
+    const [attachmentMenu, setAttachmentMenu] = useState(false);
+    const [reverseRecordingAnimation, setReverseRecording] = useState(false);
+    const [reverseAnimationAttachmentMenu, setReverseAnimationAttachmentMenu] =
+      useState(false);
+    const [reverseActivityAnimation, setReverseActivityAnimation] =
+      useState(false);
+    const [typing, setTyping] = useState(false);
+    const [input, setInput] = useState("");
+    const inputRef = useRef(null);
 
-        const closeAttachmentMenu = () => {
-            if (reverseAnimationAttachmentMenu) {
-                setReverseAnimationAttachmentMenu(false);
-                setAttachmentMenu(false);
-            }
-        };
+    const closeAttachmentMenu = () => {
+      if (reverseAnimationAttachmentMenu) {
+        setReverseAnimationAttachmentMenu(false);
+        setAttachmentMenu(false);
+      }
+    };
 
-        const closeActivityContainer = () => {
-            if (reverseActivityAnimation) {
-                setReverseActivityAnimation(false);
-                setActivity(false);
-            }
-        };
+    const closeActivityContainer = () => {
+      if (reverseActivityAnimation) {
+        setReverseActivityAnimation(false);
+        setActivity(false);
+      }
+    };
 
-        const stopRecording = () => {
-            if (reverseRecordingAnimation) {
-                setReverseRecording(false);
-                setRecording(false);
-            }
-        };
+    const stopRecording = () => {
+      if (reverseRecordingAnimation) {
+        setReverseRecording(false);
+        setRecording(false);
+      }
+    };
 
-        const sendMsg = () => {
-            // @ts-ignore
-            const msg = inputRef.current.innerText.replaceAll("\n", "<br/>");
-        };
+    const sendMsg = () => {
+      // @ts-ignore
+      const msg = inputRef.current.innerText.replaceAll("\n", "<br/>");
+    };
 
-        const handleAttachments = (e: any) => {
-            if (attachmentModal.files[0]) {
-                resetAttachmentModal();
-            }
-            setReverseAnimationAttachmentMenu(true);
-            setAttachmentModal(activeChat?.chatInfo.id);
-            addAttachments(e.target?.files);
-        };
+    const handleAttachments = (e: any) => {
+      if (attachmentModal.files[0]) {
+        resetAttachmentModal();
+      }
+      setReverseAnimationAttachmentMenu(true);
+      setAttachmentModal(activeChat?.chatInfo.id);
+      addAttachments(e.target?.files);
+    };
 
-        const attachmentsArray = [
-            <PictureIcon className={s.pictureIcon}>
-                <input
-                    onChange={handleAttachments}
-                    multiple={true}
-                    type="file"
-                    accept="image/png"
+    const attachmentsArray = [
+      <PictureIcon className={s.pictureIcon}>
+        <input
+          onChange={handleAttachments}
+          multiple={true}
+          type="file"
+          accept="image/png"
+        />
+      </PictureIcon>,
+      <CameraIcon className={s.cameraIcon} />,
+      <DocumentIcon className={s.docIcon}>
+        <input
+          onChange={handleAttachments}
+          type="file"
+          multiple={true}
+          accept=".docx, .doc, .pdf, .zip, .rar"
+        />
+      </DocumentIcon>,
+      <AvatarIcon className={s.avatarIcon} />,
+      <VideoCallIcon className={s.videoIcon} />,
+    ];
+
+    return (
+      <>
+        {activity ? (
+          <Activity
+            ref={inputRef}
+            typing={typing}
+            setTyping={setTyping}
+            setInput={setInput}
+            onClose={closeActivityContainer}
+            reverseActivityAnimation={reverseActivityAnimation}
+          />
+        ) : null}
+        <div
+          style={{
+            height: height === 0 ? 45 : 29 + height,
+          }}
+          className={s.footer}
+        >
+          <div className={s.footerControls}>
+            {activity ? (
+              <ExpandOptions reverse={reverseActivityAnimation}>
+                <CloseIcon
+                  onClick={() => setReverseActivityAnimation(true)}
+                  className="icons"
                 />
-            </PictureIcon>,
-            <CameraIcon className={s.cameraIcon} />,
-            <DocumentIcon className={s.docIcon}>
-                <input
-                    onChange={handleAttachments}
-                    type="file"
-                    multiple={true}
-                    accept=".docx, .doc, .pdf, .zip, .rar"
-                />
-            </DocumentIcon>,
-            <AvatarIcon className={s.avatarIcon} />,
-            <VideoCallIcon className={s.videoIcon} />,
-        ];
+              </ExpandOptions>
+            ) : null}
+            <SmileIcon
+              onClick={() => setActivity("emojiDrawer")}
+              className={`icons ${
+                activity === "emojiDrawer" ? "active-icon" : ""
+              }`}
+            />
 
-        return (
-            <>
-                {activity ? (
-                    <Activity
-                        ref={inputRef}
-                        typing={typing}
-                        setTyping={setTyping}
-                        setInput={setInput}
-                        onClose={closeActivityContainer}
-                        reverseActivityAnimation={reverseActivityAnimation}
-                    />
-                ) : null}
+            {activity ? (
+              <ExpandOptions reverse={reverseActivityAnimation}>
                 <div
-                    style={{
-                        height: height === 0 ? 45 : 29 + height,
-                    }}
-                    className={s.footer}
+                  style={{
+                    display: "flex",
+                  }}
                 >
-                    <div className={s.footerControls}>
-                        {activity ? (
-                            <ExpandOptions reverse={reverseActivityAnimation}>
-                                <CloseIcon
-                                    onClick={() =>
-                                        setReverseActivityAnimation(true)
-                                    }
-                                    className="icons"
-                                />
-                            </ExpandOptions>
-                        ) : null}
-                        <SmileIcon
-                            onClick={() => setActivity("emojiDrawer")}
-                            className={`icons ${
-                                activity === "emojiDrawer" ? "active-icon" : ""
-                            }`}
-                        />
-
-                        {activity ? (
-                            <ExpandOptions reverse={reverseActivityAnimation}>
-                                <GifIcon
-                                    onClick={() => setActivity("gifDrawer")}
-                                    className={`icons ${
-                                        activity === "gifDrawer"
-                                            ? "active-icon"
-                                            : ""
-                                    }`}
-                                />
-                                <StickerIcon
-                                    onClick={() => setActivity("stickerDrawer")}
-                                    className={`icons ${
-                                        activity === "stickerDrawer"
-                                            ? "active-icon"
-                                            : ""
-                                    }`}
-                                />
-                            </ExpandOptions>
-                        ) : null}
-
-                        <div className={s.attachmentButton}>
-                            {attachmentMenu ? (
-                                <ShowAttachmentAnimations
-                                    items={attachmentsArray}
-                                    className={s.attachments}
-                                    reverse={reverseAnimationAttachmentMenu}
-                                    onClose={closeAttachmentMenu}
-                                />
-                            ) : null}
-                            <AttachmentIcon
-                                onClick={() => {
-                                    if (attachmentMenu) {
-                                        setReverseAnimationAttachmentMenu(true);
-                                    } else {
-                                        setAttachmentMenu(true);
-                                    }
-                                }}
-                                className="icons"
-                            />
-                        </div>
-                    </div>
-                    <div
-                        style={{
-                            height: height === 0 ? 30 : 14 + height,
-                        }}
-                        className={s.input}
-                    >
-                        {typing ? null : (
-                            <span className={s.spanPlaceholder}>
-                                Type a message
-                            </span>
-                        )}
-                        <div className={s.spanInput}>
-                            <span
-                                ref={inputRef}
-                                contentEditable="true"
-                                data-tab="6"
-                                id="custom-input"
-                                onDrop={(e) => e.preventDefault()}
-                                onPaste={(e: any) => {
-                                    e.preventDefault();
-                                    // [Incomplete]
-                                    // setInput(e?.clipboardData?.getData("Text"));
-                                    // if (!(height === e.target?.offsetHeight)) {
-                                    //     //@ts-ignore
-                                    //     setHeight(e.target?.offsetHeight);
-                                    // }
-                                }}
-                                onInput={(e: any) => {
-                                    if (e.target.innerText.length > 0) {
-                                        setTyping(true);
-                                    } else {
-                                        setTyping(false);
-                                    }
-                                    //@ts-ignore
-                                    if (!(height === e.target?.offsetHeight)) {
-                                        //@ts-ignore
-                                        setHeight(e.target?.offsetHeight);
-                                    }
-                                }}
-                                dir="ltr"
-                                spellCheck="true"
-                                dangerouslySetInnerHTML={{ __html: input }}
-                            />
-                        </div>
-                    </div>
-                    {typing ? (
-                        <div className={`icons ${s.sendButton}`}>
-                            <SendIcon onClick={sendMsg} />
-                        </div>
-                    ) : recording ? (
-                        <RecorderAnimation
-                            onClose={stopRecording}
-                            reverse={reverseRecordingAnimation}
-                        >
-                            <AudioRecorder closeOption={setReverseRecording} />
-                        </RecorderAnimation>
-                    ) : (
-                        <div className={s.recorder}>
-                            <MicIcon
-                                onClick={() => setRecording(true)}
-                                className="icons"
-                            />
-                        </div>
-                    )}
+                  <GifIcon
+                    onClick={() => setActivity("gifDrawer")}
+                    className={`icons ${
+                      activity === "gifDrawer" ? "active-icon" : ""
+                    }`}
+                  />
+                  <StickerIcon
+                    onClick={() => setActivity("stickerDrawer")}
+                    className={`icons ${
+                      activity === "stickerDrawer" ? "active-icon" : ""
+                    }`}
+                  />
                 </div>
-            </>
-        );
-    }
+              </ExpandOptions>
+            ) : null}
+
+            <div className={s.attachmentButton}>
+              {attachmentMenu ? (
+                <ShowAttachmentAnimations
+                  items={attachmentsArray}
+                  className={s.attachments}
+                  reverse={reverseAnimationAttachmentMenu}
+                  onClose={closeAttachmentMenu}
+                />
+              ) : null}
+              <AttachmentIcon
+                onClick={() => {
+                  if (attachmentMenu) {
+                    setReverseAnimationAttachmentMenu(true);
+                  } else {
+                    setAttachmentMenu(true);
+                  }
+                }}
+                className="icons"
+              />
+            </div>
+          </div>
+          <div
+            style={{
+              height: height === 0 ? 30 : 14 + height,
+            }}
+            className={s.input}
+          >
+            {typing ? null : (
+              <span className={s.spanPlaceholder}>Type a message</span>
+            )}
+            <div className={s.spanInput}>
+              <span
+                ref={inputRef}
+                contentEditable="true"
+                data-tab="6"
+                id="custom-input"
+                onDrop={(e) => e.preventDefault()}
+                onPaste={(e: any) => {
+                  e.preventDefault();
+                  // [Incomplete]
+                  // setInput(e?.clipboardData?.getData("Text"));
+                  // if (!(height === e.target?.offsetHeight)) {
+                  //     //@ts-ignore
+                  //     setHeight(e.target?.offsetHeight);
+                  // }
+                }}
+                onInput={(e: any) => {
+                  if (e.target.innerText.length > 0) {
+                    setTyping(true);
+                  } else {
+                    setTyping(false);
+                  }
+                  //@ts-ignore
+                  if (!(height === e.target?.offsetHeight)) {
+                    //@ts-ignore
+                    setHeight(e.target?.offsetHeight);
+                  }
+                }}
+                dir="ltr"
+                spellCheck="true"
+                dangerouslySetInnerHTML={{ __html: input }}
+              />
+            </div>
+          </div>
+          {typing ? (
+            <div className={`icons ${s.sendButton}`}>
+              <SendIcon onClick={sendMsg} />
+            </div>
+          ) : recording ? (
+            <RecorderAnimation
+              onClose={stopRecording}
+              reverse={reverseRecordingAnimation}
+            >
+              <AudioRecorder closeOption={setReverseRecording} />
+            </RecorderAnimation>
+          ) : (
+            <div className={s.recorder}>
+              <MicIcon onClick={() => setRecording(true)} className="icons" />
+            </div>
+          )}
+        </div>
+      </>
+    );
+  }
 );
