4357bf5e96e43ce20171f59db1020c5abcc8da48
Joel Jaimon <joel.jaimon99@gmail.com>
2021-09-10 02:13:34 +0530

    Re-Auth implementation of user

---
+++ whatsapp-client/src/redux/sockets/handlers/socketHandlers.ts
@@ -0,0 +1,7 @@
+export const errorHandler = (event: any, emit: any) => {
+  emit(event.payload);
+};
+
+export const signInSuccess = (event: any, emit: any) => {
+  emit(event.payload);
+};
--- whatsapp-client/src/redux/store.ts
+++ whatsapp-client/src/redux/store.ts
@@ -3,6 +3,11 @@ import logger from "redux-logger";
 import { combinedReducers } from "./rootReducer";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./sagas/rootSaga";
+import { refreshToken } from "../utils/refreshToken";
+import { getAccessToken } from "../utils/accessToken";
+import { setAuthFailed, setAuthSuccess } from "./reducers/auth";
+import { getActiveSocket, initializeSocket } from "./sockets/socketConnection";
+
 const sagaMiddleware = createSagaMiddleware();
 
 const middleware = [logger, sagaMiddleware];
@@ -11,6 +16,22 @@ const store = configureStore({
   reducer: combinedReducers,
   middleware,
 });
+
+// Re-Auth of returning user
+(async () => {
+  await refreshToken();
+  const accessToken = getAccessToken();
+
+  if (!accessToken) {
+    store.dispatch(setAuthFailed(null));
+  } else {
+    await initializeSocket();
+    const socket = getActiveSocket();
+    await socket.on("signInSuccess", (payload: any) => {
+      store.dispatch(setAuthSuccess(payload));
+    });
+  }
+})();
 
 sagaMiddleware.run(rootSaga);
 
