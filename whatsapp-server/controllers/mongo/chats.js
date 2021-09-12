4336efe42fe853dca13f60c5ccb84403f9e61245
Joel Jaimon <joel.jaimon99@gmail.com>
2021-09-13 01:37:43 +0530

    getAllChats and messeges endpoint added

---
+++ whatsapp-server/controllers/mongo/chats.js
@@ -0,0 +1,51 @@
+const { ObjectID } = require("bson");
+const { mongoDB } = require("../../utils/database");
+
+exports.getGroupsChats = async (req, res) => {
+  try {
+    const db = await mongoDB().db();
+    const chats = await db
+      .collection("chats")
+      .find({
+        participants: {
+          $eq: ObjectID(req.payload._id),
+        },
+      })
+      .toArray();
+    const groups = await db
+      .collection("groups")
+      .find({
+        participants: {
+          $eq: ObjectID(req.payload._id),
+        },
+      })
+      .toArray();
+
+    const fi = [...chats, ...groups].sort((x, y) => x.timestamp - y.timestamp);
+    return res.status(201).json({
+      data: fi,
+    });
+  } catch (err) {
+    console.log(err);
+    res.send(404).end();
+  }
+};
+
+exports.getMessages = async (req, res) => {
+  try {
+    const { refId } = req.params;
+    const db = await mongoDB().db();
+    const messages = await db
+      .collection("messages")
+      .find({ refId: ObjectID(refId) })
+      .sort({ timestamp: -1 })
+      .toArray();
+
+    return res.status(201).json({
+      data: messages,
+    });
+  } catch (err) {
+    console.log(err);
+    res.send(404).end();
+  }
+};
