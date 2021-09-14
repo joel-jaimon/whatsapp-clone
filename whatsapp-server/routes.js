3c7e99bfb79e440b190ba538daaa2bfc911483f3
Joel Jaimon <joel.jaimon99@gmail.com>
2021-09-14 12:00:38 +0530

    ! clean up

--- whatsapp-server/routes.js
+++ whatsapp-server/routes.js
@@ -10,36 +10,42 @@ const multer = require("multer");
 const {
   handleFileUpload,
   handleGetResource,
-  handleGetResourceFromServer,
 } = require("./controllers/s3/handlers");
 var router = express.Router();
 
+// temp upload location for files
 const uploadLocation = multer({ dest: "uploads/" });
 
-router.get("/", (req, res) => {
+// check if server is active
+router.get("/", (_, res) => {
   res.json({
     active: true,
   });
 });
 
+// refresh access token and update the refresh token
 router.get("/refresh-token", sendRefreshToken);
 
+// signin with google
 router.post("/g-auth/authenticate", googlelogin);
 
+// logout user
 router.get("/logout", logout);
 
+// get messages of a particular chat, group
 router.get("/chats/:refId", isAuthREST, getMessages);
 
+// get chats, groups for a particular user
 router.get("/chats", isAuthREST, getGroupsChats);
 
+// upload files
 router.post(
   "/file-upload/:fileType",
   uploadLocation.single("whatsapp-clone-message-file"),
   handleFileUpload
 );
 
+// chat message files
 router.get("/resources/:fileType/:key", handleGetResource);
-
-// router.get("/resources/server/get/:key", handleGetResourceFromServer);
 
 module.exports = router;
