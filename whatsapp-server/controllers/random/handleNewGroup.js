5164c70d190cac72f9872e2a3ac4d659b34738a7
Joel Jaimon <joel.jaimon99@gmail.com>
2021-09-21 01:44:43 +0530

    group creation endpoint added

--- whatsapp-server/controllers/random/handleNewGroup.js
+++ whatsapp-server/controllers/random/handleNewGroup.js
@@ -1,8 +1,34 @@
+const { ObjectId } = require("bson");
 const { mongoDB } = require("../../utils/database");
 
 const handleNewGroup = async (req, res) => {
   const db = await mongoDB().db();
-  const { tokenId } = req.body;
+  const { _id, participants, modifiedOn, type, desc, createdOn, avatar, name } =
+    req.body;
+
+  console.log(req.body);
+
+  if (!_id) {
+    return res.sendStatus(404);
+  }
+
+  await db.collection("groups").insertOne({
+    _id: ObjectId(_id),
+    participants: participants.map((participant) => {
+      return {
+        objectId: ObjectId(participant.objectId),
+        lastViewed: participant.lastViewed,
+      };
+    }),
+    modifiedOn,
+    type,
+    desc,
+    createdOn,
+    avatar,
+    name,
+  });
+
+  return res.sendStatus(200);
 };
 
 module.exports = { handleNewGroup };
